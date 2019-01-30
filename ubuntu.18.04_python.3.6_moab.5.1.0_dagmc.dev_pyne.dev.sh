437176ce89cadef23c6d03571fb01c7f56e5b999
Mouginot B <mouginot.baptiste@gmail.com>
2019-01-30 15:46:43 -0600

    more generic script

--- ubuntu.18.04_python.3.6_moab.5.1.0_dagmc.dev_pyne.dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t ubuntu.18.04_py:3.6 -f ubuntu/u.18.04.py2.dockerfile .
-docker build -t ubuntu.18.04_python.3.6_moab:5.1.0 -f moab/u.18.04_py.3.6_moab.5.1.0.dockerfile .
-docker build -t ubuntu.18.04_python.3.6_moab.5.1.0_dagmc:dev -f dagmc/u.18.04_py.3.6_moab.5.1.0_dagmc.dev.dockerfile .
-docker build -t ubuntu.18.04_py.3.6_moab.5.1.0_dagmc.dev_pyne:dev -f pyne/u.18.04_py.3.6_moab.5.1.0_dagmc.dev_pyne.dev.dockerfile .
-
